02ddef61993295242f89e2107d394015f43d6c55
Hiroto Iguchi <sutaminajing40@gmail.com>
2024-09-10 11:04:58 +0900

    AIS-21-データの保存先をdataフォルダからtmpフォルダに変更

--- src/services/utils/constants.ts
+++ src/services/utils/constants.ts
@@ -1,3 +1,6 @@
-export const PDF_SAVE_DIR = "data/pdfs";
-export const FAISS_INDEX_DIR = "data/faiss";
+import path from "path";
+import os from "os";
+
+export const PDF_SAVE_DIR = path.join(os.tmpdir(), "pdfs");
+export const FAISS_INDEX_DIR = path.join(os.tmpdir(), "faiss");
 export const PORT = 3000;
