43e75d8a6727cf27faf8fa919b610c669df50db0
Hiroto Iguchi <sutaminajing40@gmail.com>
2024-09-10 10:59:58 +0900

    AIS-21-PDF保存先のパス指定を変更

--- src/services/utils/constants.ts
+++ src/services/utils/constants.ts
@@ -1,3 +1,3 @@
-export const PDF_SAVE_DIR = "./data/pdfs";
-export const FAISS_INDEX_DIR = "./data/faiss";
+export const PDF_SAVE_DIR = "data/pdfs";
+export const FAISS_INDEX_DIR = "data/faiss";
 export const PORT = 3000;
